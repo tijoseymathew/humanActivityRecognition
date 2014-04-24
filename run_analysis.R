d73c3bc6b36957ff1510d2c71284371a33950fa5
Josey Mathew <tijoseymathew@gmail.com>
2014-04-24 18:40:36 +0800

    Completed cleaning data

---
+++ run_analysis.R
@@ -0,0 +1,38 @@
+## General comments
+
+## TODO
+# Check for files and download if necessary
+
+featureNames <- read.table("UCI HAR Dataset\\features.txt") #Read the feature names
+activityNames <- read.table("UCI HAR Dataset\\activity_labels.txt") #Read the Activity names
+
+testData <- read.table("UCI HAR Dataset\\test\\X_test.txt") #Read the test data
+testSubject <- read.table("UCI HAR Dataset\\test\\subject_test.txt") #Read subject id for test data
+testY <- read.table("UCI HAR Dataset\\test\\y_test.txt") #Read activity id for test data
+
+trainData <- read.table("UCI HAR Dataset\\train\\X_train.txt") #Read the train data
+trainSubject <- read.table("UCI HAR Dataset\\train\\subject_train.txt") #Read subject id for train data
+trainY <- read.table("UCI HAR Dataset\\train\\y_train.txt") #Read activity id for train data
+
+mergedData <- rbind(trainData, testData) #Merge test and train data
+colnames(mergedData) <- featureNames[,2] #Name columns by feature names
+#cbind(Subject=rbind(trainSubject, testSubject), mergedData) #Add column for subject id
+mergedData$SubjectID <- c(trainSubject[[1]], testSubject[[1]]) #Add column for subject id
+mergedData$ActivityID <- factor( c(trainY[[1]], testY[[1]]), 
+                                    levels=activityNames[,1], 
+                                    labels=activityNames[,2]) #Add column for target Y(activity)
+
+meanColNames <- grep("mean", colnames(mergedData), value=TRUE) #Column names with mean in it
+stdColNames <- grep("std", colnames(mergedData), value=TRUE) #Column names with std in it
+subMergedData <- mergedData[, c(meanColNames,stdColNames, "SubjectID", "ActivityID")] #Subset of mergedData with only 
+                                                                                  #mean, std, subject id & Activityid
+
+selIDX = !(names(subMergedData) %in% c("SubjectID","ActivityID")) #Logical vector selecting all col. except SubjectID, 
+                                                                  #ActivityID
+summData <- sapply( split(subMergedData, 
+                          interaction( subMergedData$SubjectID, subMergedData$ActivityID),
+                          drop=TRUE), #Splits the df on sublevels of SubjectID, ActivityID
+                    function(elm){
+                      apply(elm[,selIDX], 2, mean) #Takes the mean of each col. in selIDX for each SubjectID, ActivityID
+                    } )
+summData <- t(summData)
